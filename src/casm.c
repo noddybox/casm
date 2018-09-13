b442eb4bbab3820ce6bc0b2551c1c560c2911c43
Ian C <ianc@noddybox.co.uk>
2018-09-13 10:21:23 +0100

    Updated version for next development cycle.

--- src/casm.c
+++ src/casm.c
@@ -59,7 +59,7 @@
 */
 
 static const char *casm_usage =
-"Version 1.5\n"
+"Version 1.6 Development\n"
 "\n"
 "This program is distributed in the hope that it will be useful,\n"
 "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
